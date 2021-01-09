6415b907a6db7c0855a9fd569b80655f260e08f5
Joseph Abraham <ekurubal24@gmail.com>
2021-01-09 21:13:45 +0300

    added email

--- src/cblog/settings.py
+++ src/cblog/settings.py
@@ -136,3 +136,13 @@ LOGIN_REDIRECT_URL = "blog:list"
 
 LOGIN_URL = "login"
 
+# sending email
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config("EMAIL_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_PASSWORD")
+
+
