77185b43f5673e52f97c7af76302f4d6b6750f98
Joseph Abraham <ekurubal24@gmail.com>
2021-01-09 21:38:06 +0300

    added email check on forgot password

--- src/users/forms.py
+++ src/users/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, PasswordResetForm
 from django.forms import fields
 from .models import Profile
 
@@ -13,7 +13,7 @@ class RegistrationForm(UserCreationForm):
         fields = ("username", "email")
 
     def clean_email(self):
-        email = self.cleaned_data['email']  # HENRY@GMAİL.COM
+        email = self.cleaned_data['email']  
         if User.objects.filter(email=email).exists():
             raise forms.ValidationError(
                 "Please use another Email, that one already taken")
@@ -36,3 +36,12 @@ class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ("username", "email")
+
+
+class PasswordResetEmailCheck(PasswordResetForm):
+
+    def clean_email(self):
+        email = self.cleaned_data["email"]
+        if not User.objects.filter(email=email).exists():
+            raise forms.ValidationError("There is no email")
+        return email
